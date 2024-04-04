8c5313d5415e283f9273099e6bc6c4418d34887b
ownleex <ownleex@gmail.com>
2024-04-05 01:10:27 +0200

    Mise à jour de la fonction ft_atoi dans client.c

--- src/client.c
+++ src/client.c
@@ -6,34 +6,33 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 01:21:47 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/04/04 21:32:20 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/04/05 01:09:55 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static int	ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	int					i;
-	int					sign;
-	unsigned long int	result;
+	int	result;
+	int	sign;
+	int	i;
 
-	i = 0;
-	sign = 1;
 	result = 0;
-	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+	sign = 1;
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
-	if (str[i] == '-')
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
 	{
 		sign = -1;
 		i++;
 	}
-	else if (str[i] == '+')
-		i++;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		result *= 10;
-		result += str[i] - '0';
+		result = result * 10 + str[i] - '0';
 		i++;
 	}
 	return (result * sign);
