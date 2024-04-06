5db066d835ff86b69ce2ce9d76e52e580d4f8ca5
ownleex <ownleex@gmail.com>
2024-04-06 02:29:10 +0200

    Mise à jour de la fonction ft_atob dans client.c

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 01:21:47 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/04/06 02:28:15 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/04/06 02:29:05 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ void	ft_atob(int pid, char c)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(30);
+		usleep(50);
 		bit++;
 	}
 }
