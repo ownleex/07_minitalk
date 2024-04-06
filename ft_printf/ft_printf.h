53777ba9adaf1a29bf970bfe5055a0b30e713b6e
ownleex <ownleex@gmail.com>
2024-04-06 03:16:18 +0200

    Mise à jour de la fonction ft_atob dans client.c - Réduction du délai d'attente entre les signaux

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 15:02:09 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/04/05 00:56:01 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/04/06 03:06:07 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ int		ft_printchar(char c);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 size_t	ft_strlen(const char *str);
+char	*ft_itoa(int n);
 
 #endif
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 01:21:57 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/04/06 00:18:49 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/04/06 03:15:30 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@ void	ft_btoa(int sig)
 {
 	static int	bit;
 	static int	i;
+	char		c;
 
 	if (sig == SIGUSR1)
 		i |= (0x01 << bit);
 	bit++;
 	if (bit == 8)
 	{
-		ft_printf("%c", i);
+		c = (char)i; // Cast i en char
+		write(1, &c, 1);
+		//ft_printf("%c", i);
 		bit = 0;
 		i = 0;
 	}
