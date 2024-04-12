e6d188c44c92ade4deeb03f3cd6733344a897eb6
ownleex <ownleex@gmail.com>
2024-04-12 22:04:53 +0200

    Mise à jour du délai d'attente entre les signaux dans la fonction ft_atob du fichier client.c et client_bonus.c

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 01:21:47 by ayarmaya          #+#    #+#             */
-/*   Updated: 2024/04/12 22:01:02 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2024/04/12 22:04:48 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ static void	ft_atob(int pid, char c)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(90);
+		usleep(150);
 		bit++;
 	}
 }
